2b4fca3127d35a3f31423ef7b23c6c971bd3c8e1
Matrix <154484064+InvisableMatrix@users.noreply.github.com>
2026-03-05 15:11:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,6 +18,7 @@ vless://6b7b8f5c-b40b-4aba-acbd-72f7b53a0888@162.159.49.59:443?encryption=none&s
 vless://6b7b8f5c-b40b-4aba-acbd-72f7b53a0888@198.41.193.52:443?encryption=none&security=tls&sni=fn.watermatrix.eu.org&fp=random&type=ws&host=fn.watermatrix.eu.org&path=%2F%3Fed%3D2560#Main%204
 vless://6b7b8f5c-b40b-4aba-acbd-72f7b53a0888@198.41.208.233:443?encryption=none&security=tls&sni=fn.watermatrix.eu.org&fp=random&type=ws&host=fn.watermatrix.eu.org&path=%2F%3Fed%3D2560#Main%205
 ss://bm9uZTowZjE0MmNkOS0wOGI2LTQ4NTEtYjk0Mi1mNTE3OGVhZmYwMDc@198.41.195.110:443?plugin=v2ray-plugin%3Bmode%3Dwebsocket%3Bhost%3Dwasmer.redsalt.us.ci%3Bpath%3D%2F0f142cd9%3Btls%3Bmux%3D0#Wasmer
+vless://0f142cd9-08b6-4851-b942-f5178eaff007@162.159.27.151:443?encryption=none&security=tls&sni=wasmer.redsalt.us.ci&fp=chrome&insecure=1&allowInsecure=1&type=ws&host=wasmer.redsalt.us.ci&path=%2F0f142cd9#Wasmer-SG
 trojan://0f142cd9-08b6-4851-b942-f5178eaff007@198.41.195.110:443?security=tls&sni=wasmer.redsalt.us.ci&fp=chrome&insecure=1&allowInsecure=1&type=ws&host=wasmer.redsalt.us.ci&path=%2F0f142cd9#Wasmer
 vless://0f142cd9-08b6-4851-b942-f5178eaff007@198.41.195.110:443?encryption=none&security=tls&sni=wasmer.redsalt.us.ci&fp=chrome&insecure=1&allowInsecure=1&type=ws&host=wasmer.redsalt.us.ci&path=%2F0f142cd9#Wasmer
 vless://0f142cd9-08b6-4851-b942-f5178eaff007@bia.redmatrix.eu.org:443?encryption=none&security=tls&sni=bia.rEDmatRIx.EU.OrG&alpn=http%2F1.1&fp=chrome&type=ws&host=bia.redmatrix.eu.org&path=%2FeyJqdW5rIjoib2ZEUnluaDkyenhLUjh5IiwicHJvdG9jb2wiOiJ2bCIsIm1vZGUiOiJwcm94eWlwIiwicGFuZWxJUHMiOlsibmEuODc3Nzc0Lnh5eiIsImNmLjg3Nzc3NC54eXoiLCJmcmVleXguY2xvdWRmbGFyZTg4LmV1Lm9yZyIsInNhYXMuc2luLmZhbiIsImN0LjA5MDIyNy54eXoiXX0%3D#%F0%9F%92%A6%2001
@@ -901,6 +902,7 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 	}
 
 }
+
 
 
 
